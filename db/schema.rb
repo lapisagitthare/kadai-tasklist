5ebb62467419cce09d4794f9d1a3cf549b27362e
kazuya.kunichika <lapisagitt.hare@gmail.com>
2020-07-22 10:44:59 +0000

    add val, col

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_045402) do
+ActiveRecord::Schema.define(version: 2020_07_22_100332) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "status"
   end
 
 end
