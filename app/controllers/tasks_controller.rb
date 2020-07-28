f035c5eda52085f80a1e7e4a36f77a77637af5c5
kazuya.kunichika <lapisagitt.hare@gmail.com>
2020-07-28 08:19:19 +0000

    add login session

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+   before_action :require_user_logged_in, only: [:index]
+   before_action :correct_user, only: [:show, :update, :destroy]
   def index
     @tasks = Task.all
   end
@@ -12,14 +14,14 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
-      redirect_to @task
+      redirect_to root_url
     else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Task が投稿されませんでした'
-    render :new
+      render :new
     end
   end
 
@@ -52,5 +54,12 @@ class TasksController < ApplicationController
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
